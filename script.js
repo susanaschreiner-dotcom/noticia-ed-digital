96d9f52811f1787775ce7720a7a57aa73f98ae03
susanaschreiner-dotcom <susana.schreiner@escola.pr.gov.br>
2026-08-12 10:56:39 -0300

    noticia

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const botao = document.getElementById("botao");
 
+botao.addEventListener("click", function () {
+    document.querySelector("article").classList.toggle("destaque");
+
+    if (document.querySelector("article").classList.contains("destaque")) {
+        botao.textContent = "Remover destaque";
+    } else {
+        botao.textContent = "Clique para destacar a notícia";
+    }
+});
